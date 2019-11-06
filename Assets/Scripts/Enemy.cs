39509842504d57dfd40bb684f44adb7c0de25616
master agod <agodsouruintan3nd@gmail.com>
2019-11-06 12:23:43 +0900

    first-commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour{
+    public float speed =5.0f;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.Rotate(1f,1f,1f);
+        // deltaTime掛けると1秒間計算
+        // 回転時World座標系に
+        transform.Translate(0,0,-speed*Time.deltaTime,Space.World);
+        if (transform.position.z < -30.0f) {
+            Destroy(gameObject);
+        }
+    }
+}
